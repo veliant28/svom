f4c17c0083fad513b7e485dfd3a083f4d2cf20d3
VS <veliant28@gmail.com>
2026-04-23 09:53:31 +0300

    Fix realtime chat startup with ASGI in docker backend

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -38,6 +38,7 @@ DEBUG = False
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 INSTALLED_APPS = [
+    "daphne",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
